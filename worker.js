0e8f3148cc8e087d700a6b24caee31c3d946c93e
Aarav <goelaarav38@gmail.com>
2026-08-03 01:39:36 +0530

    Add app tutorials redirect

--- worker.js
+++ worker.js
@@ -1,5 +1,14 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/app_tutorials" || url.pathname === "/app_tutorials/") {
+      return Response.redirect(
+        "https://drive.google.com/drive/folders/1sqvi_gx5YjGuF1VCUn9n7HH1EY-EXN7_?usp=sharing",
+        302,
+      );
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
